ac11b27500bfe68c7c7eac83f657c46ecada2725
Daniel Cruz <danielcunha54@gmail.com>
2022-07-17 21:18:04 -0300

    Setup basic express app and lint script

--- packages/invoices/src/index.ts
+++ packages/invoices/src/index.ts
@@ -1,3 +1,17 @@
-import { isOdd } from '@monorepo/core'
+import cors from 'cors'
+import express from 'express'
 
-console.log(`Is 7 odd? ${isOdd(7)}`)
+import { isEven } from '@monorepo/core'
+
+const app = express()
+app.use(cors())
+
+app.get('/', (_request, response) => {
+  const timestamp = Date.now()
+  console.log(`Is ${timestamp} even? ${isEven(timestamp)}`)
+
+  return response.status(204).send()
+})
+
+const { PORT = 3000 } = process.env
+app.listen(PORT, () => console.log(`Invoices service is running on port ${PORT}`))
